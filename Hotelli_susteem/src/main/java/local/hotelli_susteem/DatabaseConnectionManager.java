36e3b80d54b9e52ab027aa62e87e38e0b9fc04ca
KarlJug <98745340+KarlJug@users.noreply.github.com>
2022-11-25 18:07:06 +0200

    Add files via upload

---
+++ Hotelli_susteem/src/main/java/local/hotelli_susteem/DatabaseConnectionManager.java
@@ -0,0 +1,56 @@
+package local.hotelli_susteem;
+
+/*
+    nimi parool jne. teha läbi properties faili
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnectionManager {
+
+    public Connection databaseLink;
+
+    private final String url;
+    private final Properties properties;
+
+    public DatabaseConnectionManager(String host, String databaseName,
+                                     String username, String password) {
+
+        this.url = "jdbc:mysql://" + host + "/" + databaseName;
+        this.properties = new Properties();
+        this.properties.setProperty("user", username);
+        this.properties.setProperty("password", password);
+
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(this.url, this.properties);
+    }
+
+
+    /*
+    public Connection getDatabaseLink() {
+        String databaseName = "test_info";
+        String databaseUser = "root";
+        String databasePassword = "Passw0rd";
+        String url = "jdbc:mysql://localhost/" + databaseName;
+
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return databaseLink;
+    }
+     */
+
+
+}
--- Hotelli_susteem/src/main/java/local/hotelli_susteem/HotelController.java
+++ Hotelli_susteem/src/main/java/local/hotelli_susteem/HotelController.java
@@ -1,18 +1,67 @@
 package local.hotelli_susteem;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
 
-public class HotelController {
+public class HotelController implements Initializable {
 
-    public void sisene_button(ActionEvent e) {
-        System.out.println("click on button");
+
+    public void connectioneCheck(ActionEvent event) {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "test_info", "root", "Passw0rd");
+        try {
+            Connection connection = dcm.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select UserName from UserAccount");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        }   catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void hover_on_sisene_button(ActionEvent e) {
-        System.out.println("HOVER");
+
+
+    @FXML
+    VBox verbox;
+
+    // Pildid
+    InputStream stream1 = getClass().getResourceAsStream("icons/rooms.png");
+    InputStream stream2 = getClass().getResourceAsStream("icons/login.png");
+    InputStream stream3 = getClass().getResourceAsStream("icons/userFile.png");
+
+    // info mis on võetud andmebaasist ja näitab seda
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        double with = verbox.getPrefWidth();
+        //.setSpacing(2)
+
+        verbox.getChildren().add(new Button("Ruum 1", new HBox(
+                new ImageView(new Image(stream1)),
+                new Line(10.0f, 10.0f, 10.0f, 30.0f),
+                new ImageView(new Image(stream2)),
+                new Line(10.0f, 10.0f, 10.0f, 30.0f),
+                new ImageView(new Image(stream3))
+        )));
+
     }
-
-
 
 }
